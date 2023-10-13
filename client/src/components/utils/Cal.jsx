6c57dcc1b3c0b03513368fc06e08b7ddde942fa2
Utkarsh Kadam <99534647+Utkarsh-kadam@users.noreply.github.com>
2023-10-13 19:21:11 +0530

    cal modal removed

--- client/src/components/utils/Cal.jsx
+++ client/src/components/utils/Cal.jsx
@@ -4,29 +4,8 @@ import Navbar from './Navbar';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
-import Modal from 'react-modal';
 import { useMediaQuery } from 'react-responsive';
 
-
-
-Modal.setAppElement("#root");
-const customModalStyles = {
-    overlay: {
-        backgroundColor: "rgba(0, 0, 0, 0.9)", // Background color behind the modal
-      },
-    content: {
-        top: '30%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        zIndex: 999999,
-        marginRight: '-50%',
-        backgroundColor: "rgba(222, 233, 233,1)",
-        color:'#000000',
-        transform: 'translate(-50%, -50%)',
-        
-    },
-};
 
 
 
@@ -34,7 +13,6 @@ const localizer = momentLocalizer(moment);
 
 function Cal() {
     const [events, setEvents] = useState([]);
-    const [selectedEvent, setSelectedEvent] = useState(null);
     const isMobile = useMediaQuery({ maxWidth: 768 }); 
 
 
@@ -47,6 +25,7 @@ function Cal() {
                     start: new Date(event.startDate), // Use 'start' instead of 'startDate'
                     end: new Date(event.endDate), // Use 'end' instead of 'endDate'
                     title: event.name,
+                    venue: event.venue,
                 }));
                 setEvents(sortedEvents);
             })
@@ -55,13 +34,14 @@ function Cal() {
             });
     }, []);
 
-    const handleSelectEvent = (event) => {
-        setSelectedEvent(event);
-    };
-
-    const closeModal = () => {
-        setSelectedEvent(""); // Close the modal by resetting the selected event state
-    };
+    const CustomEvent = ({ event }) => (
+        <div>
+          <strong>{event.title}</strong>
+          <p className='cal_venue'>{event.venue}</p>
+        </div>
+      );
+      
+   
 
 
     return (
@@ -77,28 +57,9 @@ function Cal() {
                 endAccessor="end"
                 defaultView={isMobile ? 'agenda' : 'month'}
                 style={{ height: 550 ,width:"80vw", margin:"auto" }}
-                selectable
-                onSelectEvent={handleSelectEvent}
+                components={{ event: CustomEvent }}
             />
 
-                <Modal
-                isOpen={!!selectedEvent}
-                onRequestClose={closeModal}
-                style={customModalStyles}
-            >
-                {selectedEvent && (
-                    <div>
-                        
-                        <p>Event Name: {selectedEvent.name}</p>
-                        <p>Venue: {selectedEvent.venue} </p>
-
-                        <br/>
-                        <button className='button' onClick={closeModal}>Close</button>
-                    
-                       
-                    </div>
-                )}
-            </Modal>
         </div>
         </div>
     );
